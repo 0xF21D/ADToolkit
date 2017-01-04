a9502a06593f0d8d971abfdf21947984faddfe71
0xF21D <robert@roberthollingshead.net>
2017-01-04 07:09:25 -0600

    Add files via upload

---
+++ security_failure_analysis.ps1
@@ -0,0 +1,304 @@
+﻿#requires -Modules ActiveDirectory
+#requires -Version 2.0
+
+# The OU that your domain controllers reside in.
+$DomainControllerOU = 'ou=Domain Controllers,dc={your},dc={domain}'
+
+# The path of the resulting HTML file. 
+$filepath = '{path} + {filename}.html'
+
+# Email server IP addresses.
+$emailserverips = @('10.3.7.143', '10.3.7.144', '10.3.7.145', '10.3.7.146', '10.3.7.147', '10.3.7.148', '10.3.7.149', '10.3.7.150')
+
+$timestamp = (Get-Date).addminutes(-30)
+$domaincontrollers = (Get-ADComputer -SearchBase $DomainControllerOU -Filter *).name
+
+
+
+$scriptblock = {
+  param($timestamp)
+
+  $eventlist = Get-WinEvent @{
+    logname   = 'security';
+    starttime = $($timestamp);
+    keywords  = 4503599627370496;
+  }
+
+  [array]$report = $null
+
+  foreach ($event in $eventlist) 
+  {
+    [xml]$eventxml = $null
+    $reportitem = New-Object -TypeName psobject
+
+    $eventxml = [xml]$event.toxml()
+
+    $reportitem | Add-Member -MemberType NoteProperty -Name EventID -Value $($event.Id)
+    $reportitem | Add-Member -MemberType NoteProperty -Name TimeCreated -Value $($event.TimeCreated)
+
+    switch ($event.Id) {
+
+
+      4625 # An account failed to log on.
+      {
+        $reportitem | Add-Member -MemberType NoteProperty -Name FailureDescription -Value 'Login Failure'
+        $reportitem | Add-Member -MemberType NoteProperty -Name FailureReason -Value ''
+        $reportitem | Add-Member -MemberType NoteProperty -Name TargetUserName -Value $($eventxml.Event.EventData.Data[5].'#text')
+        $reportitem | Add-Member -MemberType NoteProperty -Name TargetDomainName -Value $($eventxml.Event.EventData.Data[6].'#text')
+        $reportitem | Add-Member -MemberType NoteProperty -Name WorkstationName -Value $($eventxml.Event.EventData.Data[13].'#text').replace('\\','')
+        $reportitem | Add-Member -MemberType NoteProperty -Name IPAddress -Value $($eventxml.Event.EventData.Data[19].'#text')
+
+        if ($reportitem.LogonType -eq '3') 
+        {
+          $reportitem.LogonType = 'Network'
+        }
+
+        # Get code from status in XML.
+        switch ($eventxml.Event.EventData.Data[8].'#text') {
+        
+          '%%2313' 
+          {
+            $reportitem.FailureReason = 'Username/Password Bad'
+          }
+
+          '%%2310' 
+          {
+            $reportitem.FailureReason = 'Account Disabled'
+          }
+
+          '%%2304' 
+          {
+            $reportitem.FailureReason = 'Error Occured'
+          }
+
+          '%%2309' 
+          {
+            $reportitem.FailureReason = 'Password Expired'
+          }
+
+          '%%2307' 
+          {
+            $reportitem.FailureReason = 'Account Locked Out'
+          }
+          
+        
+        }
+
+        $report = $report + $reportitem
+      }
+
+      4768 # A Kerberos authentication ticket (TGT) was requested.
+      {
+        $reportitem | Add-Member -MemberType NoteProperty -Name FailureDescription -Value 'Kerberos TGT Request Failure'
+        $reportitem | Add-Member -MemberType NoteProperty -Name FailureReason -Value ''
+        $reportitem | Add-Member -MemberType NoteProperty -Name TargetUserName -Value $($eventxml.Event.EventData.Data[0].'#text')
+        $reportitem | Add-Member -MemberType NoteProperty -Name TargetDomainName -Value $($eventxml.Event.EventData.Data[1].'#text')
+        $reportitem | Add-Member -MemberType NoteProperty -Name WorkstationName -Value ''
+        $reportitem | Add-Member -MemberType NoteProperty -Name IPAddress -Value $($eventxml.Event.EventData.Data[9].'#text').replace('::ffff:','')   
+        
+         
+        # Get code from status in XML.
+        switch ($eventxml.Event.EventData.Data[6].'#text') {
+          '0x12' 
+          {
+            $reportitem.FailureReason = 'Account Locked/Expired/Disabled'
+          }
+
+          '0x17' 
+          {
+            $reportitem.FailureReason = 'Password Expired'
+          }
+
+          '0x6' 
+          {
+            $reportitem.FailureReason = 'Bad Username'
+          }
+
+          '0x18' 
+          {
+            $reportitem.FailureReason = 'Bad Password'
+          }
+
+          '0x25' 
+          {
+            $reportitem.FailureReason = 'Time Skew'
+          }
+
+        }
+      
+      
+
+      
+        $report = $report + $reportitem
+      }
+
+
+      
+      4771 # Kerberos pre-authentication failed.
+      {
+        $reportitem | Add-Member -MemberType NoteProperty -Name FailureDescription -Value 'Kerberos Pre-Authentication Failure'
+        $reportitem | Add-Member -MemberType NoteProperty -Name FailureReason -Value ''
+        $reportitem | Add-Member -MemberType NoteProperty -Name TargetUserName -Value $($eventxml.Event.EventData.Data[0].'#text')
+        $reportitem | Add-Member -MemberType NoteProperty -Name TargetDomainName -Value ''
+        $reportitem | Add-Member -MemberType NoteProperty -Name WorkstationName -Value ''
+        $reportitem | Add-Member -MemberType NoteProperty -Name IPAddress -Value $($eventxml.Event.EventData.Data[6].'#text').replace('::ffff:','')   
+        
+        # Get code from status in XML.
+        switch ($eventxml.Event.EventData.Data[4].'#text') {
+          '0x12' 
+          {
+            $reportitem.FailureReason = 'Account Locked/Expired/Disabled'
+          }
+
+          '0x17' 
+          {
+            $reportitem.FailureReason = 'Password Expired'
+          }
+
+          '0x6' 
+          {
+            $reportitem.FailureReason = 'Bad Username'
+          }
+
+          '0x18' 
+          {
+            $reportitem.FailureReason = 'Bad Password'
+          }
+
+          '0x25' 
+          {
+            $reportitem.FailureReason = 'Time Skew'
+          }
+
+
+        
+      
+        }
+
+
+        $report = $report + $reportitem
+      }
+
+    }
+  }
+
+  return $report
+}
+
+$jobid = (Invoke-Command -AsJob -ComputerName $domaincontrollers -Credential exterran\rhollin01-da -ScriptBlock $scriptblock -ArgumentList $timestamp).id
+
+while ((Get-Job -Id $jobid).State -eq 'Running') 
+{
+  Start-Sleep -Seconds 5
+  Get-Job -Id $jobid
+}
+
+$endtime = Get-Date
+
+$eventlist = Receive-Job -Id $jobid
+Remove-Job -Id $jobid
+
+[array]$scoreboard = $null
+
+$firstrun = $true
+
+foreach ($event in $eventlist) 
+{
+  if ($event.eventid) 
+  {
+    $score = New-Object -TypeName PSObject
+    $found = $false
+
+
+    if ($event.ipaddress -in $emailserverips) 
+    { 
+      $event | Add-Member -MemberType NoteProperty -Name Hostname -Value 'Email Server Pool'
+      $event.IPAddress = 'Multiple'
+    } else 
+    {
+      $event | Add-Member -MemberType NoteProperty -Name Hostname -Value 'Unresolved'
+    }
+
+   
+
+    if (!$firstrun) 
+    {
+      foreach ($entry in $scoreboard) 
+      {
+        if ($entry.IPAddress -eq $event.ipaddress -and $entry.SamAccountName -eq $event.TargetUserName -and $entry.FailureDescription -eq $event.FailureDescription -and $entry.FailureReason -eq $event.FailureReason) 
+        {
+          $entry.EntryCount++  
+          $found = $true
+          break
+        }
+      }
+    }
+  
+
+    if (!$found -and !$firstrun) 
+    {
+      if ($event.ipaddress -ne 'Multiple') 
+      { 
+        try 
+        {
+          $score | Add-Member -MemberType NoteProperty -Name Hostname -Value $([system.net.dns]::GetHostbyAddress($($event.IPAddress)).HostName)
+        } catch 
+        {
+          $score | Add-Member -MemberType NoteProperty -Name Hostname -Value 'Unresolved'
+        }
+      } else 
+      {
+        $score | Add-Member -MemberType NoteProperty -Name Hostname -Value $($event.hostname)
+      }
+
+
+
+      $score | Add-Member -MemberType NoteProperty -Name IPAddress -Value $($event.ipaddress)
+      $score | Add-Member -MemberType NoteProperty -Name SamAccountName -Value $($event.TargetUserName)
+      $score | Add-Member -MemberType NoteProperty -Name FailureDescription -Value $($event.FailureDescription)
+      $score | Add-Member -MemberType NoteProperty -Name FailureReason -Value $($event.FailureReason)
+      $score | Add-Member -MemberType NoteProperty -Name EntryCount -Value 1 
+      $scoreboard = $scoreboard + $score
+    }
+
+
+    if ($firstrun) 
+    {
+      if ($event.ipaddress -ne 'Multiple') 
+      { 
+        try 
+        {
+          $score | Add-Member -MemberType NoteProperty -Name Hostname -Value $([system.net.dns]::GetHostbyAddress($($event.IPAddress)).HostName)
+        } catch 
+        {
+          $score | Add-Member -MemberType NoteProperty -Name Hostname -Value 'Unresolved'
+        }
+      } else 
+      {
+        $score | Add-Member -MemberType NoteProperty -Name Hostname -Value $($event.hostname)
+      }
+      $score | Add-Member -MemberType NoteProperty -Name IPAddress -Value $($event.ipaddress)
+      $score | Add-Member -MemberType NoteProperty -Name SamAccountName -Value $($event.TargetUserName)
+      $score | Add-Member -MemberType NoteProperty -Name FailureDescription -Value $($event.FailureDescription)
+      $score | Add-Member -MemberType NoteProperty -Name FailureReason -Value $($event.FailureReason)
+      $score | Add-Member -MemberType NoteProperty -Name EntryCount -Value 1
+      $firstrun = $false
+      $scoreboard = $scoreboard + $score
+    }
+  }
+}
+
+$Header = '<html><head><title>Active Directory Security Failure Scoreboard</title></head><body><h1>All failures from ' + $timestamp + ' to ' +$endtime + ' (~30 minutes).</h1><p><pre>'
+$Footer = '</pre></p></body></html>'
+
+$Header |
+Format-Table |
+Out-File -Width 256 -FilePath $filepath
+$scoreboard |
+Sort-Object -Property SamAccountName |
+Format-Table |
+Out-File -Width 256 -FilePath $filepath -Append
+$Footer |
+Sort-Object -Property TimeCreated |
+Format-Table |
+Out-File -Width 256 -FilePath $filepath -Append
